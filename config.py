8863ca49c3c1296eb83575e641b3b642f8d2edc5
deatherz192 <106407810+deatherz192@users.noreply.github.com>
2023-05-14 11:14:23 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,57 @@
+######################################################
+# PLEASE CHANGE FOLLOWING CONFIGS ####################
+Twitch_Channel          = 'deatherz192'
+
+Trans_Username          = 'ditugga_bot'
+#Trans_Username          = 'ddotb'
+Trans_OAUTH             = 'oauth:kvl59h3dc696kupu5a7s3zfzawtix3'
+#Trans_OAUTH             = 'oauth:asrboztzet3ybctrrun47rnlfhhbs5'
+
+#######################################################
+# OPTIONAL CONFIGS ####################################
+Trans_TextColor         = 'HotPink'
+# Blue, Coral, DodgerBlue, SpringGreen, YellowGreen, Green, OrangeRed, Red, GoldenRod, HotPink, CadetBlue, SeaGreen, Chocolate, BlueViolet, and Firebrick
+
+lang_TransToHome        = 'ru'
+lang_HomeToOther        = 'ru'
+
+Show_ByName             = True
+Show_ByLang             = True
+
+Ignore_Lang             = ["tt", "af", "sq", "am", "ar", "hy", "az", "eu", "be", "bn", "bs", "bg", "ca", "ceb", "ny", "zh-CN", "zh-TW", "co",
+                "hr", "cs", "da", "nl", "eo", "et", "tl", "fi", "fy", "gl", "ka", "de", "el", "gu", "ht", "ha",
+                "haw", "iw", "hi", "hmn", "hu", "is", "ig", "id", "ga", "ja", "jw", "kn", "kk", "km", "ko", "ku", "ky",
+                "lo", "la", "lv", "lt", "lb", "mk", "mg", "ms", "ml", "mt", "mi", "mr", "mn", "my", "ne", "no", "ps", "fa",
+                "pl", "pt", "ma", "ro", "ru", "sm", "gd", "sr", "st", "sn", "sd", "si", "sk", "sl", "so", "su", "sw",
+                "sv", "tg", "ta", "te", "th", "tr", "uk", "ur", "uz", "vi", "cy", "xh", "yi", "yo", "zu"]
+Ignore_Users            = ['Nightbot', 'ditugga_bot']
+Ignore_Line             = ['http', '.com', '.ru' '⣿', '░', '▓']
+Delete_Words            = ['ditugga']
+
+# Any emvironment, set it to `True`, then text will be read by TTS voice!
+# TTS_In:User Input Text, TTS_Out:Bot Output Text
+TTS_In                  = False
+TTS_Out                 = False
+TTS_Kind                = "gTTS" # You can choice "CeVIO" if you want to use CeVIO as TTS.
+# CeVIO_Cast            = "さとうささら" # When you are using CeVIO, you must set voice cast name.
+TTS_TextMaxLength		= 0
+TTS_MessageForOmitting = ""
+
+# if you make TTS for only few lang, please add langID in the list
+# for example, ['ja'] means Japanese only, ['ko','en'] means Korean and English are TTS!
+ReadOnlyTheseLang       = []
+
+# Select the translate engine ('deepl' or 'google')
+Translator              = 'deepl'
+
+# Use Google Apps Script for tlanslating
+# e.g.) GAS_URL         = 'https://script.google.com/macros/s/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx/exec'
+GAS_URL                 = ''
+
+# Enter the suffix of the Google Translate URL you normally use.
+# Example: translate.google.co.jp -> 'co.jp'
+#          translate.google.com   -> 'com'
+GoogleTranslate_suffix  = 'com'
+
+# If you meet any bugs, You can check some error message using Debug mode (Debug = True)
+Debug                   = True
